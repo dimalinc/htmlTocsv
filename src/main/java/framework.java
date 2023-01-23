1ecbbd4abb1311ada4dfba5371d9984fae660a1f
dimalinc <linc@ua.fm>
2023-01-23 18:05:34 +0200

    MultithreadingDemo extends Thread

--- src/main/java/framework.java
+++ src/main/java/framework.java
@@ -15,38 +15,138 @@ import java.util.stream.Stream;
 public class framework {
 
     static class MultithreadingDemo extends Thread {
+        private final String fileString;
+
+        MultithreadingDemo(String fileString) {
+            this.fileString = fileString;
+        }
+
         public void run()
         {
             try {
-                // Displaying the thread that is running
-                System.out.println(
-                        "Thread " + Thread.currentThread().getId()
-                                + " is running");
+                n = n + 1;
+
+                input = new File(filesFolder + fileString);
+                System.out.println(n + "___" + fileString);
+                doc=docInit(input);
+
+                listOfListsOfElements = new ArrayList<>();
+
+
+                for(String xpathString:xpathStringsList) {
+                    ArrayList<Element> jsoupElementsList = Xsoup.compile(xpathString).evaluate(doc).getElements();
+                    listOfListsOfElements.add(jsoupElementsList);
+                }
+
+           /* ArrayList<Element> jsoupElementsList9 = Xsoup.compile("//h1").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList9.size(); i++) {
+                //  System.out.println("------------");
+                //  System.out.println(jsoupElementsList9.get(i).text());
+                //  System.out.println("------------");
+            }
+            listOfListsOfElements.add(jsoupElementsList9);
+            ArrayList<Element> jsoupElementsList0 = Xsoup.compile("//span[@class='price']").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList0.size(); i++) {
+                // System.out.println(jsoupElementsList0.get(i).text());
+                // System.out.println("-----");
+            }
+            listOfListsOfElements.add(jsoupElementsList0);
+
+            ArrayList<Element> jsoupElementsList1 = Xsoup.compile("//span[contains(@class, 'amstockstatus')]").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList1.size(); i++) {
+                //   System.out.println(jsoupElementsList1.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList1);
+            ArrayList<Element> jsoupElementsList2 = Xsoup.compile("//div[@itemprop='sku']").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList2.size(); i++) {
+                //  System.out.println(jsoupElementsList2.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList2);
+            ArrayList<Element> jsoupElementsList3 = Xsoup.compile("//img[starts-with(@src,'https://trail-gear.com/media/catalog/')]").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList3.size(); i++) {
+                //   System.out.println(jsoupElementsList3.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList3);
+            ArrayList<Element> jsoupElementsList4 = Xsoup.compile("//div[@class='product attribute description']").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList4.size(); i++) {
+                //  System.out.println(jsoupElementsList4.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList4);
+            //
+            ////h2[contains(text(),'Specifications')]
+            ArrayList<Element> jsoupElementsList5 = Xsoup.compile("//div[@id='specstab']/ul/li").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList5.size(); i++) {
+                //   System.out.println(jsoupElementsList5.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList5);
+            //[contains(text(),'Application Data')]
+            ArrayList<Element> jsoupElementsList6 = Xsoup.compile("//div[@id='apptab']/ul/li").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList6.size(); i++) {
+                //   System.out.println(jsoupElementsList6.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList6);
+            ArrayList<Element> jsoupElementsList7 = Xsoup.compile("//div[@id='instructionstab']/a/@href").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList7.size(); i++) {
+                //   System.out.println(jsoupElementsList7.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList7);
+            ArrayList<Element> jsoupElementsList8 = Xsoup.compile("//div[@class='data item content']").evaluate(doc).getElements();
+            for (int i = 0; i < jsoupElementsList8.size(); i++) {
+                // System.out.println(jsoupElementsList8.get(i).text());
+            }
+            listOfListsOfElements.add(jsoupElementsList8);*/
+
+                //page link
+                elementsStringArrayOneRow[0] = "https://wheelersoffroad.com/" + fileString;
+                for (int i = 0; i < listOfListsOfElements.size(); i++) {
+                    for (Element element : listOfListsOfElements.get(i)) {
+                        // img links
+                        if ((i == 4) && (element.attr("src").length() > 0)) {
+                            cellContents.append(element.attr("src")).append(System.getProperty("line.separator"));
+                        }
+
+                        if (((i != 4) || (i != 8)) /*&& (element.text().length() > 0)*/) {
+                            //  System.out.println(element.text());
+                            cellContents.append(element.text()).append(System.getProperty("line.separator"));
+                        }
+                        // else if ( (i!=4) && (element.text().length() == 0) ) cellContents.append("_").append(System.getProperty("line.separator"));
+
+                        if ((i == 8) && (element.attr("href").length() > 0)) {
+                            //  System.out.println("+++ 8th_element href =" + element.attr("href"));
+                            cellContents = new StringBuilder();
+                            cellContents.append(element.attr("href")).append(System.getProperty("line.separator"));
+                        }
+                    }
+                    elementsStringArrayOneRow[i + 1] = cellContents.toString();
+                }
+
+                arrayListOfAllStringsForCSV.add(elementsStringArrayOneRow);
+                // if (n>200) break;
             }
             catch (Exception e) {
-                // Throwing an exception
-                System.out.println("Exception is caught");
+                e.printStackTrace();
             }
         }
     }
 
+    static ArrayList<String[]> arrayListOfAllStringsForCSV = new ArrayList<>();
     static ArrayList<String> xpathStringsList = new ArrayList<>();
     static String[] elementsStringArrayOneRow = new String[11];
     static StringBuilder cellContents = new StringBuilder();
     static File input=null;
     static int n = 0;
     private static ArrayList<String> xpathStringsListinit() {
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
-        xpathStringsList.add("");
+        xpathStringsList.add("//h1");
+        xpathStringsList.add("//span[@class='price']");
+        xpathStringsList.add("//span[contains(@class, 'amstockstatus')]");
+        xpathStringsList.add("//div[@itemprop='sku']");
+        xpathStringsList.add("/img[starts-with(@src,'https://trail-gear.com/media/catalog/')]");
+        xpathStringsList.add("//div[@class='product attribute description']");
+        xpathStringsList.add("//div[@id='specstab']/ul/li");
+        xpathStringsList.add("//div[@id='apptab']/ul/li");
+        xpathStringsList.add("//div[@id='instructionstab']/a/@href");
+        xpathStringsList.add("//div[@class='data item content']");
+       // xpathStringsList.add("");
         return xpathStringsList;
     }
     static String filesFolder = "D:\\Wheelersoffroad_BACKUP\\savedHtml_Replaced" + "\\";
@@ -107,116 +207,23 @@ public class framework {
         /*System.out.println("filesStringsListInDir " + filesStringsListInDir.size());
         System.out.println("--------");*/
 
-        ArrayList<String[]> arrayListOfAllStringsForCSV = new ArrayList<>();
         csvWriter=csvWriterInit();
 
         // TODO: добавить многопоточность
-            /*int n = 3; // Number of threads
-            for (int i = 0; i < n; i++) {
-                MultithreadingDemo object
-                        = new MultithreadingDemo();
-                object.start();
-            }*/
+
+        for (int i = 0; i < 1; i++) {
         for (String fileString : filesStringsListInDir) {
 
-            n = n + 1;
+                MultithreadingDemo object
+                        = new MultithreadingDemo(fileString);
+                object.start();
 
-            input = new File(filesFolder + fileString);
-            System.out.println(n + "___" + fileString);
-            doc=docInit(input);
-
-            listOfListsOfElements = new ArrayList<>();
-
-
-            for(String xpathString:xpathStringsList) {
-                ArrayList<Element> jsoupElementsList = Xsoup.compile(xpathString).evaluate(doc).getElements();
-                listOfListsOfElements.add(jsoupElementsList);
-            }
-
-           /* ArrayList<Element> jsoupElementsList9 = Xsoup.compile("//h1").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList9.size(); i++) {
-                //  System.out.println("------------");
-                //  System.out.println(jsoupElementsList9.get(i).text());
-                //  System.out.println("------------");
-            }
-            listOfListsOfElements.add(jsoupElementsList9);
-            ArrayList<Element> jsoupElementsList0 = Xsoup.compile("//span[@class='price']").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList0.size(); i++) {
-                // System.out.println(jsoupElementsList0.get(i).text());
-                // System.out.println("-----");
-            }
-            listOfListsOfElements.add(jsoupElementsList0);
-
-            ArrayList<Element> jsoupElementsList1 = Xsoup.compile("//span[contains(@class, 'amstockstatus')]").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList1.size(); i++) {
-                //   System.out.println(jsoupElementsList1.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList1);
-            ArrayList<Element> jsoupElementsList2 = Xsoup.compile("//div[@itemprop='sku']").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList2.size(); i++) {
-                //  System.out.println(jsoupElementsList2.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList2);
-            ArrayList<Element> jsoupElementsList3 = Xsoup.compile("//img[starts-with(@src,'https://trail-gear.com/media/catalog/')]").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList3.size(); i++) {
-                //   System.out.println(jsoupElementsList3.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList3);
-            ArrayList<Element> jsoupElementsList4 = Xsoup.compile("//div[@class='product attribute description']").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList4.size(); i++) {
-                //  System.out.println(jsoupElementsList4.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList4);
-            //
-            ////h2[contains(text(),'Specifications')]
-            ArrayList<Element> jsoupElementsList5 = Xsoup.compile("//div[@id='specstab']/ul/li").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList5.size(); i++) {
-                //   System.out.println(jsoupElementsList5.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList5);
-            //[contains(text(),'Application Data')]
-            ArrayList<Element> jsoupElementsList6 = Xsoup.compile("//div[@id='apptab']/ul/li").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList6.size(); i++) {
-                //   System.out.println(jsoupElementsList6.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList6);
-            ArrayList<Element> jsoupElementsList7 = Xsoup.compile("//div[@id='instructionstab']/a/@href").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList7.size(); i++) {
-                //   System.out.println(jsoupElementsList7.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList7);
-            ArrayList<Element> jsoupElementsList8 = Xsoup.compile("//div[@class='data item content']").evaluate(doc).getElements();
-            for (int i = 0; i < jsoupElementsList8.size(); i++) {
-                // System.out.println(jsoupElementsList8.get(i).text());
-            }
-            listOfListsOfElements.add(jsoupElementsList8);*/
-
-            //page link
-            elementsStringArrayOneRow[0] = "https://wheelersoffroad.com/" + fileString;
-            for (int i = 0; i < listOfListsOfElements.size(); i++) {
-                for (Element element : listOfListsOfElements.get(i)) {
-                    // img links
-                    if ((i == 4) && (element.attr("src").length() > 0)) {
-                        cellContents.append(element.attr("src")).append(System.getProperty("line.separator"));
-                    }
-
-                    if (((i != 4) || (i != 8)) /*&& (element.text().length() > 0)*/) {
-                        //  System.out.println(element.text());
-                        cellContents.append(element.text()).append(System.getProperty("line.separator"));
-                    }
-                    // else if ( (i!=4) && (element.text().length() == 0) ) cellContents.append("_").append(System.getProperty("line.separator"));
-
-                    if ((i == 8) && (element.attr("href").length() > 0)) {
-                      //  System.out.println("+++ 8th_element href =" + element.attr("href"));
-                        cellContents = new StringBuilder();
-                        cellContents.append(element.attr("href")).append(System.getProperty("line.separator"));
-                    }
+                try {
+                    object.join(400);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                elementsStringArrayOneRow[i + 1] = cellContents.toString();
             }
-
-            arrayListOfAllStringsForCSV.add(elementsStringArrayOneRow);
-            // if (n>200) break;
         }
 
         System.out.println("CSV writing STARTED " + (System.currentTimeMillis() - start) / 1000 + " seconds"
